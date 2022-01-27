b909821a5b4ea266b8a3b7b98648cb640351da9f
Jing Li <lijing900913@gmail.com>
2022-01-27 16:39:52 -0500

    CRUD for post with its id

---
+++ public/js/updatePost.js
@@ -0,0 +1,43 @@
+const updatePost = async (event) => {
+    const name = document.querySelector('#title').value.trim();
+    const description = document.querySelector('#content').value.trim();
+    const id = document.querySelector('#current-post-id').value.trim();
+  
+    if (name && description) {
+      const response = await fetch('/api/post/'+id, {
+        method: 'PUT',
+        body: JSON.stringify({ name, description}),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update.');
+      }
+    }
+  };
+
+  const deletePost = async (event) => {
+    const id = document.querySelector('#current-post-id').value.trim();
+  
+    if (id) {
+      const response = await fetch('/api/post/'+id, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete.');
+      }
+    }
+  };
+  
+  document
+  .querySelector('#update-post')
+  .addEventListener('click', updatePost);
+
+  document
+  .querySelector('#delete-post')
+  .addEventListener('click', deletePost);
+
+  
